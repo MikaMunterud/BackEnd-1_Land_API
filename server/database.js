c043e9ea4f5c9351dafd07aa9282c2a0b87decce
Mika Munterud <mika.munterud@outlook.com>
2023-03-02 12:34:07 +0100

    add gitignore

---
+++ server/database.js
@@ -0,0 +1,42 @@
+module.exports.users = [
+  { username: "Mika", password: "Choklad" },
+  { username: "Alexander", password: "Choklad" },
+];
+module.exports.countries = [
+  {
+    country: "Sweden",
+    population: 10481937,
+    capital: "Stockholm",
+    language: "Swedish",
+  },
+  {
+    country: "China",
+    population: 1411750000,
+    capital: "Beijing",
+    language: "Mandarin",
+  },
+  {
+    country: "Nicaragua",
+    population: 6600000,
+    capital: "Managua",
+    language: "Spanish",
+  },
+  {
+    country: "South Africa",
+    population: 60604992,
+    capital: "Cape Town",
+    language: "Zulu",
+  },
+  {
+    country: "United Kingdom",
+    population: 67791400,
+    capital: "London",
+    language: "English",
+  },
+  {
+    country: "Canada",
+    population: 39292355,
+    capital: "Toronto",
+    language: "English",
+  },
+];
